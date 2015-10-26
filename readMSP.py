42acf1a3d8408d7843cd89ff9d2e938255ddfe64
scienceopen <scienceopen@users.noreply.github.com>
2015-10-26 12:01:34 -0400

    zoom

--- readMSP.py
+++ readMSP.py
@@ -5,11 +5,16 @@ from pandas import Panel
 from os.path import expanduser
 from numpy import arange
 from matplotlib.pyplot import subplots,show
+from datetime import datetime,timedelta
+from dateutil.parser import parse
 #import seaborn
 
-def readmsp(fn):
+def readmsp(fn,date):
     with Dataset(expanduser(fn),'r') as f:
         secdayutc=f.variables['Time'][:]
+        t = [parse(date)+ timedelta(seconds=int(s)) for s in secdayutc]
+
+
         Analog=f.variables['AnalogData'][:]
 
         Ipeak=f.variables['PeakIntensity'][:]
@@ -18,32 +23,41 @@ def readmsp(fn):
         wavelen=f.variables['Wavelength'][:]
         elv=arange(181.) #TODO is this right?
 
-        Intensity = Panel(items=secdayutc,major_axis=wavelen,minor_axis=elv,data=Ipeak)
+        Intensity = Panel(items=t,major_axis=wavelen,minor_axis=elv,data=Ipeak)
         return Intensity
 
-def plotmspspectra(Intensity):
+def plotmspspectra(Intensity,tlim):
     #unpack Panel
     wavelen=Intensity.major_axis.values
     elv = Intensity.minor_axis.values
-    secdayutc=Intensity.items.values
+    t=Intensity.items.to_pydatetime()
     Ipeak = Intensity.values
     #%% plots
-    fg,ax = subplots(6,1,figsize=(15,5))
+    fg,ax = subplots(6,1,figsize=(20,12))
+    fg.suptitle(t[0].strftime('%Y-%m-%d'))
     for i,(a,l) in enumerate(zip(ax,wavelen)):
-        a.pcolormesh(secdayutc,elv,Ipeak[:,i,:].T,cmap='cubehelix')
+        h=a.pcolormesh(t,elv,Ipeak[:,i,:].T,cmap='cubehelix')
+        fg.colorbar(h,ax=a)
         a.set_title('{:.1f} nm'.format(l))
         a.set_ylabel('elevation [deg.]')
         a.autoscale(True,'x',tight=True)
+        if tlim:
+            xlim = [parse(l) for l in tlim]
+            a.set_xlim(xlim)
 
-    a.set_xlabel('sec since UTC midnight')
+    a.set_xlabel('UTC')
+
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='reading Poker Flat Research Range Meridian Scanning Photometer')
     p.add_argument('ncfn',help='netCDF data file name to read')
+    p.add_argument('date',help='date of file YYYYMMDD') #FIXME: regexp filename
+    p.add_argument('--tlim',help='time window to zoom plot in on',nargs=2)
     p = p.parse_args()
 
-    Intensity = readmsp(p.ncfn)
-    plotmspspectra(Intensity)
+    Intensity = readmsp(p.ncfn,p.date)
+    plotmspspectra(Intensity,None)
+    plotmspspectra(Intensity,p.tlim) #zoom
 
     show()
