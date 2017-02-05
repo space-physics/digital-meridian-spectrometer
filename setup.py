d4a67419a759a519beb21a4c991f1dcfd6aada5a
scienceopen <scienceopen@users.noreply.github.com>
2017-02-05 03:24:59 -0500

    pythonic

--- setup.py
+++ setup.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python
-import subprocess
 from setuptools import setup
 
-try:
-    subprocess.call(['conda','install','--file','requirements.txt'])
-except Exception:
-    pass
-
-
 setup(name='msp_aurora',
-	  description='MSP Aurora Spectrometer Reader netCDF4 / netCDF3',
-	  author='Michael Hirsch',
-	  url='https://github.com/scienceopen/meridian-spectrometer-reader',
-      install_requires=['histutils','isrutils'],
+      install_requires=['nose','netCDF4','xarray','numpy','matplotlib','python-dateutil','pytz',
+        'histutils','isrutils'],
       dependency_links = [
         'https://github.com/scienceopen/histutils/tarball/master#egg=histutils',
         'https://github.com/scienceopen/isrutils/tarball/master#egg=isrutils',],
