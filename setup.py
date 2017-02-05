c72c44ea3c1b69aab8c50c935ca7332f1774f710
scienceopen <scienceopen@users.noreply.github.com>
2017-02-05 03:37:39 -0500

    try conda first

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+try:
+    import conda.cli
+    conda.cli.main('install','--file','requirements.txt')
+except Exception as e:
+    print(e)
+    import pip
+    pip.main(['install','-r','requirements.txt'])
+    
+
 setup(name='msp_aurora',
       install_requires=['nose','netCDF4','xarray','numpy','matplotlib','python-dateutil','pytz',
         'histutils','isrutils'],
