e6fb4afcd495f746f4f7993f55f8089d86bf46ae
scienceopen <scienceopen@users.noreply.github.com>
2017-02-05 03:38:45 -0500

    fallback

--- setup.py
+++ setup.py
@@ -8,11 +8,10 @@ except Exception as e:
     print(e)
     import pip
     pip.main(['install','-r','requirements.txt'])
-    
+
 
 setup(name='msp_aurora',
-      install_requires=['nose','netCDF4','xarray','numpy','matplotlib','python-dateutil','pytz',
-        'histutils','isrutils'],
+      install_requires=['histutils','isrutils'],
       dependency_links = [
         'https://github.com/scienceopen/histutils/tarball/master#egg=histutils',
         'https://github.com/scienceopen/isrutils/tarball/master#egg=isrutils',],
