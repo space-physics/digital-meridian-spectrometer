595cef47ccd59be42cf83f03d9a3d2cd253e11c7
scienceopen <scienceopen@users.noreply.github.com>
2017-02-10 17:03:35 -0500

    pythonic

--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-try:
-    import conda.cli
-    conda.cli.main('install','--file','requirements.txt')
-except Exception as e:
-    print(e)
-    import pip
-    pip.main(['install','-r','requirements.txt'])
-
+req = ['sciencedates',
+       'isrutils',
+       'nose','netCDF4','xarray','numpy','matplotlib','python-dateutil','pytz']
 
 setup(name='msp_aurora',
       packages=['msp_aurora'],
       author='Michael Hirsch, Ph.D.',
       url='https://github.com/scienceopen/meridian-spectrometer-reader',
       version='0.5',
-      install_requires=['sciencedates',
-                        'isrutils'],
+      install_requires=req,
       dependency_links = [
         'https://github.com/scienceopen/isrutils/tarball/master#egg=isrutils',],
 
