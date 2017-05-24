00e59deceffc83fd7e0bfc4dd893e8b1ded08fda
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-05-24 13:57:53 -0400

    template

--- README.rst
+++ README.rst
@@ -1,7 +1,7 @@
 .. image:: https://zenodo.org/badge/DOI/10.5281/zenodo.167565.svg
    :target: https://doi.org/10.5281/zenodo.167565
-.. image:: https://travis-ci.org/scienceopen/meridian-spectrometer-reader.svg?branch=master
-    :target: https://travis-ci.org/scienceopen/meridian-spectrometer-reader
+.. image:: https://travis-ci.org/scivision/meridian-spectrometer-reader.svg?branch=master
+    :target: https://travis-ci.org/scivision/meridian-spectrometer-reader
 
 ============================
 meridian-spectrometer-reader
--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
-from setuptools import setup
+req = ['nose','netCDF4','xarray','numpy','matplotlib','python-dateutil','pytz']
+pipreq=['sciencedates',]
 
-req = ['sciencedates',
-       'nose','netCDF4','xarray','numpy','matplotlib','python-dateutil','pytz']
+import pip
+try:
+    import conda.cli
+    conda.cli.main('install',*req)
+except Exception as e:
+    pip.main(['install']+req)
+pip.main(['install']+pipreq)
+# %%
+from setuptools import setup
 
 setup(name='msp_aurora',
       packages=['msp_aurora'],
       author='Michael Hirsch, Ph.D.',
       url='https://github.com/scivision/meridian-spectrometer-reader',
       version='0.5',
-      install_requires=req,
 	  )
 
